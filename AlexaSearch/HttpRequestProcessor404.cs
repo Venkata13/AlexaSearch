0eea44cf60ba2ad6b5e834c75d948828c11d379a
User <User@DESKTOP-P8RS130>
2021-08-24 16:59:38 +0530

    Add project files.

---
+++ AlexaSearch/HttpRequestProcessor404.cs
@@ -0,0 +1,26 @@
+﻿using Sitecore.Data.Items;
+using Sitecore.Pipelines.HttpRequest;
+
+namespace AlexaSearch
+{
+    public class HttpRequestProcessor404 : HttpRequestProcessor
+    {
+        public override void Process(HttpRequestArgs args)
+        {
+            if (Sitecore.Context.Item != null || Sitecore.Context.Site == null || Sitecore.Context.Database == null
+                || Sitecore.Context.Database.Name == "core"
+                || args.RequestUrl.AbsoluteUri.ToLower().Contains("/sitecore/api/layout/render/jss")
+                || args.RequestUrl.AbsoluteUri.ToLower().Contains("/sitecore/api/jss/import")
+                || args.RequestUrl.AbsoluteUri.ToLower().Contains("/api/sitecore/"))
+            {
+                return;
+            }
+            var pageNotFound = Sitecore.Context.Database.GetItem(@"{AEC32B57-496D-4ACF-A49D-C08FACB6EFA8}");
+            if (pageNotFound == null)
+                return;
+            args.ProcessorItem = pageNotFound;
+            Sitecore.Context.Item = pageNotFound;
+            args.HttpContext.Response.StatusCode = 404;
+        }
+    }
+}
